3656fa80e92ebdd2295fb9f015af540524ad1b0f
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-07-04 07:33:33 +0300

    Add callback function

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 console.log(`Before`);
 
-// Example of a none-blocking-function
-setTimeout(() => {
-  console.log(`Reading user from DB...`);
-}, 2000);
+// Callback
+getUser(1, getRepositories);
+
+function displayCommits(commits) {
+  console.log(commits);
+}
+
+function gitCommits(repos) {
+  getCommits(repos[1], displayCommits);
+}
+
+function getRepositories(user) {
+  getRepos(user.githubUserName, gitCommits);
+}
+
+// Promises
+
+// Async/await
 
 console.log(`After`);
+
+function getUser(id, callback) {
+  // Example of a none-blocking-function
+  setTimeout(() => {
+    console.log(`Reading user from DB...`);
+    callback({ id: id, githubUserName: 'MuhannedNoman' });
+  }, 2000);
+}
+
+function getRepos(userName, callback) {
+  setTimeout(() => {
+    console.log(`Retreving repos...`);
+    callback(['repo1', 'repo2', ' repo3']);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log(`Retreving commits of ${repo}...`);
+    callback(['saha1', 'sha2', ' sha3']);
+  }, 2000);
+}
